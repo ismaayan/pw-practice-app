1f9b0fea49c129d0dc0f5016a73b6d65fd5dbc5d
Maayan <maayan14489@gmail.com>
2024-06-08 20:47:06 +0300

    upload project to github

---
+++ tests/uiComponents.spec.ts
@@ -0,0 +1,159 @@
+import {test, expect} from '@playwright/test'
+
+test.beforeEach(async({page}) => {
+    await page.goto('http://localhost:4200/')
+   
+
+})
+
+test.describe( 'form layouts page', () => {
+    test.beforeEach(async({page}) => {
+        await page.getByText('Forms').click()
+        await page.getByText('Form Layout').click()
+    
+    })
+
+    test( 'input fields', async({page}) => {
+        const usingTheGridEmailInput = page.locator( 'nb-card', {hasText:'Using the Grid'}).getByRole('textbox', {name:'Email'})
+        await usingTheGridEmailInput.fill('test@test.com')
+        await usingTheGridEmailInput.clear()
+        await usingTheGridEmailInput.pressSequentially('test@test.com', {delay: 500})
+
+        const inputValue = await usingTheGridEmailInput.inputValue()
+        expect(inputValue).toEqual('test@test.com')
+
+        await expect(usingTheGridEmailInput).toHaveValue('test@test.com')
+
+
+    })
+
+    test( 'radio buttons' , async({page}) => {
+        const usingTheGridEmailForm = page.locator( 'nb-card', {hasText:'Using the Grid'})
+        await usingTheGridEmailForm.getByLabel('Option 1').check({force:true})
+        const radioStatus = usingTheGridEmailForm.getByLabel('Option 1').isChecked()
+        expect(radioStatus).toBeTruthy()
+
+        await usingTheGridEmailForm.getByLabel('Option 2').check({force:true})
+        await page.pause()
+        expect(await usingTheGridEmailForm.getByLabel('Option 1').isChecked()).toBeFalsy()
+        expect(await usingTheGridEmailForm.getByLabel('Option 2').isChecked()).toBeTruthy()
+
+
+
+})
+
+test( 'checkboxes' , async({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    await page.getByRole('checkbox', {name:'Hide on click'}).uncheck({force:true})
+    await page.getByRole('checkbox', {name:'Prevent arising of duplicate toast'}).check({force:true})
+
+    const allBoxes = await page.getByRole('checkbox')
+    for(const box of await allBoxes.all()){
+        await box.check({force:true})
+        expect(await box.isChecked()).toBeTruthy()
+        await box.uncheck({force:true})
+        expect(await box.isChecked()).toBeFalsy()
+    }
+
+    })
+
+    test( 'lists and dropdowns' , async({page}) => {
+        const dropDownMenu = page.locator('ngx-header nb-select')
+        await dropDownMenu.click()
+
+        const optionList = page.locator('nb-option-list nb-option')
+        expect(optionList).toContainText(["Light", "Dark", "Cosmic", "Corporate"])
+        await optionList.filter({hasText: 'Cosmic'}).click()
+        const header = page.locator('nb-layout-header')
+        expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+        const colors = {
+            "light" : "rgb(255, 255, 255)",
+            "Dark" : "rgb(34, 43, 69)",
+            "Cosmic" : "rgb(50, 50, 89)",
+            "Corporate" : "rgb(255, 255, 255)"
+        }
+
+        await dropDownMenu.click()
+        for(const color in colors){
+            await optionList.filter({hasText: color}).click()
+            await expect(header).toHaveCSS('background-color',colors[color])
+            if(color !== "corporate")
+                await dropDownMenu.click()
+        }
+
+
+    })
+
+    test( 'tooltips' , async({page}) => {
+        await page.getByText('Modal & Overlays').click()
+        await page.getByText('tooltip').click()
+        const tooltipCard = await page.locator('nb-card', {hasText:'top'})
+        await tooltipCard.getByRole('button', {name: 'Top'}).hover()
+        const tooltip = await page.locator('nb-tooltip').textContent()
+        expect (tooltip).toEqual('This is a tooltip')
+
+    })
+
+    test( 'tables & data' , async({page}) => {
+        await page.getByText('Tables & Data').click()
+        await page.getByText('Smart Table').click()
+
+        page.on('dialog', dialog => {
+            expect(dialog.message()).toEqual('Are you sure you want to delete?')
+            dialog.accept()
+        })
+
+        await page.getByRole('table').locator('tr',{hasText: 'mdo@gmail.com'}).locator('.nb-trash').click()
+        await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
+    
+   
+    }) 
+
+    test( 'web table' , async({page}) => {
+        await page.getByText('Tables & Data').click()
+        await page.getByText('Smart Table').click()
+
+        //get row by any test in row
+        const targetRow = page.getByRole('row', {name:'twitter@outlook.com'})
+        await targetRow.locator('.nb-edit').click()
+        await page.locator('input-editor').getByPlaceholder('Age').clear()
+        await page.locator('input-editor').getByPlaceholder('Age').fill('35')
+        await page.locator('.nb-checkmark').click()
+
+        //get row by based on the value in the specific column
+        await page.locator('.ng2-smart-pagination-nav').getByText('2').click()
+        const targetRowById = page.getByRole( 'row', {name:'11'}).filter({has: page.locator('td').nth(1).getByText('11')})
+        await targetRowById.locator('.nb-edit').click()
+        await page.locator('input-editor').getByPlaceholder('E-mail').clear()
+        await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com')
+        await page.locator('.nb-checkmark').click()
+        await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com')
+
+        //test filter of the table
+        const ages = ['20', '30', '40', '200']
+
+        for (let age of ages){
+            await page.locator('input-filter').getByPlaceholder('Age').clear()
+            await page.locator('input-filter').getByPlaceholder('Age').fill(age)
+            const ageRows = page.locator('tbody tr')
+
+            for(let row of await ageRows.all()){
+                const cellValue = await row.locator('td').last().textContent()
+                expect(cellValue).toEqual(age)
+            }
+
+        }
+
+
+
+
+
+
+
+    })
+
+})
+
